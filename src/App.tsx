a9912e0aea4d514fccc5643a55c88425d1ca9bca
r4vesky <r4vesky@gmail.com>
2026-09-17 22:31:05 +0700

    Remove X/Twitter; Telegram buttons get calls to action
    Hero: 'Join the channel' / 'Talk to holders', stacked full width on mobile.
    Join section: 'Join our Telegram channel' / 'Talk to holders on Telegram'.
    X link, icon, JSON-LD sameAs entry and twitter:* meta tags removed.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Chart, { MarketStats, PriceCard } from './Chart.tsx'
 // Paste the contract address here at launch. Buy and chart links derive from it.
 const CA = ''
 const LINKS = {
-  x: 'https://x.com/PutinPumpToken',
   telegram: 'https://t.me/PutinPumpToken', // channel: announcements
   telegramChat: 'https://t.me/PutinPumpChat', // group: community chat
 }
@@ -70,7 +69,6 @@ const FAQ = [
 
 const ICONS = {
   check: 'M9 16.2 4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4z',
-  x: 'M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z',
   telegram:
     'M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.962 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.48.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z',
 }
@@ -241,7 +239,7 @@ const JSON_LD = JSON.stringify({
       name: 'PUTIN PUMP',
       url: `${SITE}/`,
       logo: `${SITE}/icon-192.png`,
-      sameAs: [LINKS.x, LINKS.telegram, LINKS.telegramChat],
+      sameAs: [LINKS.telegram, LINKS.telegramChat],
     },
     {
       '@type': 'FAQPage',
@@ -277,9 +275,8 @@ export default function App() {
               </a>
             </div>
             <div className="social-row">
-              <a href={LINKS.x} {...social}><Icon name="x" />X (Twitter)</a>
-              <a href={LINKS.telegram} {...social}><Icon name="telegram" />Channel</a>
-              <a href={LINKS.telegramChat} {...social}><Icon name="telegram" />Chat</a>
+              <a href={LINKS.telegram} {...social}><Icon name="telegram" />Join the channel</a>
+              <a href={LINKS.telegramChat} {...social}><Icon name="telegram" />Talk to holders</a>
             </div>
           </div>
           <div className="orb">
@@ -421,9 +418,8 @@ export default function App() {
           <h2>Join the pump</h2>
           <p>Memes, updates and the contract address, straight from the community.</p>
           <div className="cta-row">
-            <a className="btn btn-white" href={LINKS.x} {...social}><Icon name="x" />Follow on X</a>
-            <a className="btn btn-white" href={LINKS.telegram} {...social}><Icon name="telegram" />Telegram channel</a>
-            <a className="btn btn-white" href={LINKS.telegramChat} {...social}><Icon name="telegram" />Telegram chat</a>
+            <a className="btn btn-white" href={LINKS.telegram} {...social}><Icon name="telegram" />Join our Telegram channel</a>
+            <a className="btn btn-white" href={LINKS.telegramChat} {...social}><Icon name="telegram" />Talk to holders on Telegram</a>
             <a className="btn btn-green" href={buyUrl} {...external}>Buy $PUTINPUMP</a>
           </div>
         </section>
